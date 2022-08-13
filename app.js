32230513716554c4074402aa4eda1f5e7c892fd1
SanyaOdare <sanyaodare@gmail.com>
2022-08-13 02:47:54 +0100

    set anchor tags

--- app.js
+++ app.js
@@ -1,11 +1,1 @@
 // const navLink = document.querySelectorAll('a .nav-link');
-
-// navLink.addEventListener('click', activeBtn)
-
-// function activeBtn() {
-//   navLink.forEach((item) => {
-//     item.classList.remove('active')
-//   })
-//   item.classList.add('active')
-// }
-// activeBtn();
